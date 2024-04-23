3b78dd26a72be4c75a19b3fba8deca5b6a7f41f7
tevabob13 <tevabora13@gmail.com>
2024-04-23 13:32:27 +0200

    Adding example

---
+++ Modules/Microsoft365DSC/Examples/Resources/AADDirectoryConsentSettings/1-Create.ps1
@@ -0,0 +1,31 @@
+<#
+This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
+#>
+
+Configuration M365TenantConfig
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $credsCredential
+    )
+
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    Node localhost
+    {
+        AADDirectoryConsentSettings "AADDirectoryConsentSettings"
+        {
+            BlockUserConsentForRiskyApps                    = "True";
+            ConstrainGroupSpecificConsentToMembersOfGroupId = "";
+            Credential                                      = $Credscredential;
+            DisplayName                                     = "Consent Policy Settings";
+            EnableAdminConsentRequests                      = "False";
+            EnableGroupSpecificConsent                      = "True";
+            Ensure                                          = "Present";
+            Id                                              = "6b79d399-25e5-4528-ba41-219d0b32ec46";
+            IsSingleInstance                                = "Yes";
+        }
+    }
+}
