8d3821a93e477517a09f5d3a67d9eea1432c9da5
tevabob13 <tevabora13@gmail.com>
2024-04-29 14:38:19 +0200

    fix

--- Tests/test.ps1
+++ Tests/test.ps1
@@ -6,6 +6,6 @@ $Credential = Get-Credential
 
 # Exporting resources using credentials
 Export-M365DSCConfiguration -Components @("AADPasswordRuleSettings") -Credential $Credential -Path "C:\Users\tbouchet\Downloads\Test"
+Export-M365DSCConfiguration -Components @("AADDirectorySetting") -Credential $Credential -Path "C:\Users\tbouchet\Downloads\Test"
 
-
-New-M365DSCResource -ResourceName "AADDirectorySetting" -Workload "MicrosoftGraph" -CmdLetNoun "MgBetaDirectorySetting" -Path $ResourcePath -UnitTestPath $UnitTestPath -ExampleFilePath $ExamplePath -Credential $Credential
+#New-M365DSCResource -ResourceName "AADDirectorySetting" -Workload "MicrosoftGraph" -CmdLetNoun "MgBetaDirectorySetting" -Path $ResourcePath -UnitTestPath $UnitTestPath -ExampleFilePath $ExamplePath -Credential $Credential
